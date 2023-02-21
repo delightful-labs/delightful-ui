e3ec98c95bf7dfcea0e9ba08e9741a497ca35dd5
mjamesderocher <mjamesderocher@gmail.com>
2023-02-21 17:49:35 -0500

    Added Details

---
+++ src/lib/Components/Details.svelte
@@ -0,0 +1,27 @@
+<script>
+	import clickOutside from '$lib/utils/clickOutside'
+
+	export let title = ''
+	let opened = false
+</script>
+
+<details bind:open={opened} use:clickOutside on:outclick={() => (opened = false)}>
+	<summary>{title}</summary>
+	<div class="details-content">
+		<slot />
+	</div>
+</details>
+
+<style>
+	.details-content {
+		position: absolute;
+		background-color: var(--color__background);
+		padding: 1rem;
+		width: 100vw;
+		left: 50%;
+		right: 50%;
+		margin-left: -50vw;
+		margin-right: -50vw;
+		z-index: 1;
+	}
+</style>
---
+++ src/lib/utils/clickOutside.js
@@ -0,0 +1,28 @@
+//@TODO: fix types.
+//This article can help: https://stackoverflow.com/questions/64131176/svelte-custom-event-on-svelte-typescript
+
+/**
+ * 
+ * @param {HTMLDetailsElement} node
+ */
+export default function clickOutside(node) {
+
+  /**
+   * @param {MouseEvent & { target: HTMLDetailsElement }} event 
+   */
+  const handleClick = (event) => {
+    const target = event.target
+    
+    if (target && !node.contains(event.target)) {
+      node.dispatchEvent(new CustomEvent("outclick"))
+    }
+  }
+
+  document.addEventListener("click", handleClick, true)
+
+  return {
+    destroy() {
+      document.removeEventListener("click", handleClick, true)
+    }
+  }
+}

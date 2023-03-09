35e13368a44954d91e77dda9b6f66e65da045834
mjamesderocher <mjamesderocher@gmail.com>
2023-03-09 21:18:56 +0300

    Added some styles for text input

---
+++ src/lib/Components/Inputs/Text.svelte
@@ -0,0 +1,20 @@
+<script>
+  /**
+   * @type {'email'|'text'|'url'}
+   */
+  export let type = 'text'
+</script>
+
+<input type={type} />
+
+<style>
+  input {
+    box-sizing: border-box;
+    padding: var(--input-padding, 1rem);
+    color: var(--input-color, inherit);
+    min-height: var(--input-min-height, 48px);
+    border-style: var(--input-border-style, solid);
+    border-width: var(--input-border-width, 1px);
+    background-color: var(--input-background-color, transparent);
+  }
+</style>
---
+++ src/stories/Inputs/Text.stories.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+
+  import Text from '$components/Inputs/Text.svelte'
+
+</script>
+
+<Meta title="Inputs/Text" component={Text}/>
+
+<Template let:args>
+  <Text {...args} />
+</Template>
+
+<Story name="Text Input" args={{type: "text"}}/>

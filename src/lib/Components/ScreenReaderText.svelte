c3f38e57ef7da274f9eeee38162b234b6fe8be2c
mjamesderocher <mjamesderocher@gmail.com>
2023-03-10 06:56:30 +0300

    Added ScreenReaderText

---
+++ src/lib/Components/ScreenReaderText.svelte
@@ -0,0 +1,17 @@
+<span class="screen-reader-text">
+  <slot />
+</span>
+
+<style>
+  .screen-reader-text {
+    clip: rect(1px, 1px, 1px, 1px);
+    clip-path: inset(50%);
+    height: 1px;
+    margin: -1px;
+    overflow: hidden;
+    padding: 0;
+    position: absolute;
+    width: 1px;
+    word-wrap: normal !important;
+  }
+</style>
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,7 @@
 import Button from "./Components/Button.svelte"
 import Details from "./Components/Details.svelte"
 import LoadingSpinner from "./Components/LoadingSpinner.svelte"
+import ScreenReaderText from "./Components/ScreenReaderText.svelte"
 import Text from "./Components/Inputs/Text.svelte"
 
-export { Button, Details, LoadingSpinner, Text }
+export { Button, Details, LoadingSpinner, ScreenReaderText, Text }

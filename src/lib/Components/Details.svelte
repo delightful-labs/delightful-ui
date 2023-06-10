8e53da38e75b0f02bed5a3a8333e96fe8325fa53
mjamesderocher <mjamesderocher@gmail.com>
2023-06-10 07:34:22 -0500

    Added option to position Details

--- src/lib/Components/Details.svelte
+++ src/lib/Components/Details.svelte
@@ -3,20 +3,24 @@
 
 	export let title = ''
 	export let opened = false
+	export let positionAbsolutely = false
 </script>
 
 <details bind:open={opened} use:clickOutside on:outclick={() => (opened = false)}>
 	<summary>{title}</summary>
-	<div class="details-content">
+	<div class="details-content {positionAbsolutely && 'absolute'}">
 		<slot />
 	</div>
 </details>
 
 <style>
 	.details-content {
-		position: absolute;
 		background-color: var(--color__background);
 		padding: 1rem;
+	}
+
+	.absolute {
+		position: absolute;
 		width: 100vw;
 		left: 50%;
 		right: 50%;
